c7572f5ee3e8ccd556a2b19caa3254e7c9831c18
emily <emillymay@LAPTOP-FPN7UJU6>
2024-01-20 08:47:37 -0700

    feat:signup component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
 
 import './App.css'
+import Authenticate from "./authenticate";
+import SignUpForm from "./signupform";
+import React from 'react';
+
+export default function App() {
+  return (
+    <>
+      <Authenticate />
+      <SignUpForm />
+    </>
+  );
+}
 
 
  
---
+++ src/components/SignUpForm.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+
+export default function SignUpForm() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    try {
+      const response = await fetch("https://fsa-jwt-practice.herokuapp.com/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        // Log the successful response data
+        console.log("API call successful:", data);
+      } else {
+        const data = await response.json();
+        setError(data.error);
+      }
+    } catch (error) {
+      console.error("Error during API call:", error);
+    }
+  }
+
+  return (
+    <div>
+      <h2>Sign Up!</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Username:
+            <input
+              type="text"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </label>
+        </div>
+        <div>
+          <label>
+            Password:
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      {error && <p>Error: {error}</p>}
+    </div>
+  );
+}
